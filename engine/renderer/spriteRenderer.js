ac361c58871eb2355e07a175b1e3f880ced56ccb
xjxsynx <xjxsinx@gmail.com>
2025-12-30 17:02:23 -0600

    Add files via upload

--- engine/renderer/spriteRenderer.js
+++ engine/renderer/spriteRenderer.js
@@ -1,4 +1,15 @@
-export function drawSprite(ctx, sprite, x, y, w, h) {
-  if (!sprite || !sprite.complete) return;
-  ctx.drawImage(sprite, x, y, w, h);
+
+export function drawSprite(ctx, sprite, x, y, camera) {
+  if (!sprite || !sprite.image.complete) return;
+
+  const drawX = Math.floor(x - sprite.width / 2 - camera.x);
+  const drawY = Math.floor(y - sprite.height - camera.y);
+
+  ctx.drawImage(
+    sprite.image,
+    drawX,
+    drawY,
+    sprite.width,
+    sprite.height
+  );
 }
