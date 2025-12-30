4fd7ec07ea49fc313bec8057346f7e14600acdf0
xjxsynx <xjxsinx@gmail.com>
2025-12-29 21:52:33 -0600

    Add files via upload

--- engine/renderer.js
+++ engine/renderer.js
@@ -1,3 +1,27 @@
-export function drawPlayer(ctx, player, sprite) {
-  ctx.drawImage(sprite, player.x, player.y, player.size, player.size);
+
+import { player } from "./player.js";
+import { animator } from "./playerAnimator.js";
+
+const cache = {};
+
+function load(src) {
+  if (cache[src]) return cache[src];
+  const img = new Image();
+  img.src = src;
+  cache[src] = img;
+  return img;
+}
+
+export function render(ctx) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  const frame = animator.getFrame();
+  const img = load(`engine/assets/player/${frame}.png`);
+
+  if (img.complete) {
+    ctx.drawImage(img, player.x, player.y, player.size, player.size);
+  } else {
+    ctx.fillStyle = "#00ff00";
+    ctx.fillRect(player.x, player.y, player.size, player.size);
+  }
 }
