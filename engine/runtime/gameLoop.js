399981b3b2435c654f5c46610748a0284a42afaa
xjxsynx <xjxsinx@gmail.com>
2025-12-30 13:03:02 -0600

    Add files via upload

--- engine/runtime/gameLoop.js
+++ engine/runtime/gameLoop.js
@@ -1,18 +1,7 @@
+import { renderPlayer } from "../renderer/renderer.js";
 
-import { animationState } from "../core/animationState.js";
-import { renderPlayer } from "../renderer/spriteRenderer.js";
-import { player } from "../player.js";
-
-let last = performance.now();
-export function loop(ctx) {
-  const now = performance.now();
-  const delta = now - last;
-  last = now;
-
-  animationState.resolve(player);
-  animationState.tick(delta, player.frameCount || 4);
-
+export function gameLoop(ctx, player) {
   ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-  renderPlayer(ctx);
-  requestAnimationFrame(() => loop(ctx));
+  renderPlayer(ctx, player);
+  requestAnimationFrame(() => gameLoop(ctx, player));
 }
