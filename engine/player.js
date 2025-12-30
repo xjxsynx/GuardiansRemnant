3b0e6acb002199cb18bf2e76ae848cdbd59fca94
xjxsynx <xjxsinx@gmail.com>
2025-12-30 17:20:10 -0600

    Update player.js

--- engine/player.js
+++ engine/player.js
@@ -1,13 +1,40 @@
-export const Player = {
-  x: 0,
-  y: 0,
-  size: 32,
-  dir: "down",
-  speed: 2,
-  update(input) {
-    if (input.left) { this.x -= this.speed; this.dir = "left"; }
-    if (input.right) { this.x += this.speed; this.dir = "right"; }
-    if (input.up) { this.y -= this.speed; this.dir = "up"; }
-    if (input.down) { this.y += this.speed; this.dir = "down"; }
+export class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.vx = 0;
+    this.vy = 0;
+
+    this.speed = 2;
+    this.direction = "south"; // north, south, east, west
+    this.state = "idle";      // idle, walk
   }
-};
+
+  update() {
+    this.vx = 0;
+    this.vy = 0;
+
+    if (window.input?.left) {
+      this.vx = -this.speed;
+      this.direction = "west";
+    }
+    if (window.input?.right) {
+      this.vx = this.speed;
+      this.direction = "east";
+    }
+    if (window.input?.up) {
+      this.vy = -this.speed;
+      this.direction = "north";
+    }
+    if (window.input?.down) {
+      this.vy = this.speed;
+      this.direction = "south";
+    }
+
+    this.state = (this.vx || this.vy) ? "walk" : "idle";
+
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+}
