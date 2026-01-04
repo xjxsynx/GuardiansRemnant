a697aaae378cb7f31fae0dfe0192cbbc1c371a2c
xjxsynx <xjxsinx@gmail.com>
2026-01-04 17:08:18 -0600

    Update initSprites.js

--- engine/runtime/initSprites.js
+++ engine/runtime/initSprites.js
@@ -1,12 +1,12 @@
 import { loadSprite } from "../renderer/spriteLoader.js";
 
-const DIRS = ["north","south","east","west"];
+const DIRS = ["north", "south", "east", "west"];
 
 export function initSprites() {
   DIRS.forEach(dir => {
-    loadSprite(`idle_${dir}`, `/assets/player/idle_${dir}.png`);
+    loadSprite(`idle_${dir}`, `assets/player/idle_${dir}.png`);
     for (let i = 0; i < 4; i++) {
-      loadSprite(`walk_${dir}_${i}`, `/assets/player/walk_${dir}_${i}.png`);
+      loadSprite(`walk_${dir}_${i}`, `assets/player/walk_${dir}_${i}.png`);
     }
   });
 }
